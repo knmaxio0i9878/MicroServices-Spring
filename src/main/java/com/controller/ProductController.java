1faa6d7be54bfe165a45cf7ef754f9e15780972c
knmaxio0i9878 <pmakwana1908@gmail.com>
2024-09-13 18:57:52 +0530

    InsertQuery

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,18 +1,25 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.ProductEntity;
+import com.repository.ProductRepository;
 
 
 @RestController
 public class ProductController {
 
-//	@PostMapping("/products")
-//	public String newProduct(@RequestBody ProductEntity pentity) {
-////		System.out.println(pentity);
-//		return "";
-//	}
+	@Autowired
+	ProductRepository prepo;
+	
+	@PostMapping("/products")
+	public ProductEntity newProduct(@RequestBody ProductEntity pentity) {
+		System.out.println(pentity.getProductName());
+		System.out.println(pentity.getProductPrice());
+		prepo.save(pentity);
+		return pentity;
+	}
 }
