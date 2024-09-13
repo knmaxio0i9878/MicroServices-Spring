ce19893473f64cae1d9f15c5ff8029c418e920e2
knmaxio0i9878 <pmakwana1908@gmail.com>
2024-09-13 18:01:24 +0530

    1stadd

---
+++ src/main/java/com/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.entity.ProductEntity;
+
+
+@RestController
+public class ProductController {
+
+//	@PostMapping("/products")
+//	public String newProduct(@RequestBody ProductEntity pentity) {
+////		System.out.println(pentity);
+//		return "";
+//	}
+}
