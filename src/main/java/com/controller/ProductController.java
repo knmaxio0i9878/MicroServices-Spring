21289c675067f5075404dd05e61880a3791837b8
knmaxio0i9878 <pmakwana1908@gmail.com>
2024-09-18 18:51:10 +0530

    Get and GetById

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,5 +1,8 @@
 package com.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.ProductEntity;
 import com.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -22,4 +29,26 @@ public class ProductController {
 		prepo.save(pentity);
 		return pentity;
 	}
+	
+	@GetMapping("/getproduct")
+	public List<ProductEntity> getProducts() {
+		List<ProductEntity> products = prepo.findAll();
+		return products;
+		
+	}
+	
+	@GetMapping("/getproductbyid/{productId}")
+	public ProductEntity getProducts(@PathVariable("productId") Integer productId) {
+		Optional<ProductEntity> op = prepo.findById(productId);
+		if(op.isEmpty()) {
+			return null;
+		}
+		else {
+			ProductEntity pentity = op.get();
+			return pentity;
+		}
+		
+	}
+	
+	
 }
